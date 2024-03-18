19ae1b5412cc009ce7726168e6683ae905058fc4
Ashwita Pal <ashwitapal@gmail.com>
2024-03-18 11:32:00 +0530

    solved

--- log-analytics.tf
+++ log-analytics.tf
@@ -34,3 +34,51 @@ resource "azurerm_monitor_diagnostic_setting" "diagnostic_settings" {
     }
   }
 }
+
+resource "azurerm_storage_management_policy" "example" {
+  storage_account_id = "subscriptions/75223151-1800-43db-a8f3-b7fe605d3385/resourceGroups/gaurav/providers/Microsoft.Storage/storageAccounts/terraformteststacc01"
+  // storage_account_id = azurerm_storage_account.example.id
+  rule {
+    name    = "rule1"
+    enabled = true
+    filters {
+      prefix_match = ["container1/prefix1"]
+      blob_types   = ["blockBlob"]
+    }
+    actions {
+      base_blob {
+        tier_to_cool_after_days_since_modification_greater_than    = 10
+        tier_to_archive_after_days_since_modification_greater_than = 50
+        delete_after_days_since_modification_greater_than          = 100
+      }
+      snapshot {
+        delete_after_days_since_creation_greater_than = 30
+      }
+    }
+  }
+  rule {
+    name    = "rule2"
+    enabled = false
+    filters {
+      prefix_match = ["container2/prefix1", "container2/prefix2"]
+      blob_types   = ["blockBlob"]
+    }
+    actions {
+      base_blob {
+        tier_to_cool_after_days_since_modification_greater_than    = 11
+        tier_to_archive_after_days_since_modification_greater_than = 51
+        delete_after_days_since_modification_greater_than          = 101
+      }
+      snapshot {
+        change_tier_to_archive_after_days_since_creation = 90
+        change_tier_to_cool_after_days_since_creation    = 23
+        delete_after_days_since_creation_greater_than    = 31
+      }
+      version {
+        change_tier_to_archive_after_days_since_creation = 9
+        change_tier_to_cool_after_days_since_creation    = 90
+        delete_after_days_since_creation                 = 3
+      }
+    }
+  }
+}
